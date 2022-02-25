b6c2aa4e42d5729af433ee8559fe53d7fd943151
DVDPOL79 <72322125+DVDPOL79@users.noreply.github.com>
2022-02-25 13:23:45 -0500

    Create LibroCalificaciones.java

---
+++ LibrosDeCalificaciones/LibroCalificaciones.java
@@ -0,0 +1,13 @@
+package studyTool;
+
+//declaracion de una clase con un metodo
+
+public class LibroCalificaciones 
+{
+// muestra un mensaje de bienvenida al usuario de LibroCalificaciones
+public void mostrarmensaje()
+{
+	System.out.println("Bienvenido al libro de calificaciones¡");
+} //fin del metodo mostrar mensaje
+
+} //fin de la clase LibroCalificaciones
